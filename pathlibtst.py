c72bd18e0121a46630a9e0be016818f21116caa5
Ariel Feldman <ariel.feldman@swissmedical.com.ar>
2022-05-17 17:54:19 -0300

    Update files

---
+++ pathlibtst.py
@@ -0,0 +1,7 @@
+import pathlib
+
+PATH = pathlib.Path(__file__).parent
+# PATH_FOLDER = PATH.join
+PATH_FOLDER = PATH.joinpath('../datasets').resolve()
+PATH_FOLDER_FILE = PATH_FOLDER.joinpath('empresasTech.csv').resolve()
+print(PATH_FOLDER_FILE)
